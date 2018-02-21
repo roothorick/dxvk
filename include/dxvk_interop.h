c07a208e3b64bd7c62307269f67952a358f298fb
roothorick <roothorick@gmail.com>
2018-02-21 02:03:44 -0600

    Revise API, should have everything needed now

--- include/dxvk_interop.h
+++ include/dxvk_interop.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <vulkan.h>
+
+// Avoid including DirectX headers
+class D3D11Texture2D;
+
 /**
  * \brief Request instance extensions (callback)
  *
@@ -24,7 +29,13 @@ void dxvkRegisterDeviceExtCallback(char**(*cb)(VkPhysicalDevice*));
  *
  * Extracts from the passed D3D11Texture2D the VkImage behind it and additional information critical to its use.
  */
-void dxvkExtractVkImage(D3D11Texture2D tex, VkImage* img, uint32_t width, uint32_t height, uint32_t format, uint32_t sampleCt);
+void dxvkGetVulkanImage(D3D11Texture2D* tex,
+  VkImage* img,
+  uint32_t width,
+  uint32_t height,
+  uint32_t format,
+  uint32_t sampleCt
+);
 
 /**
  * \brief DXGI adapter index of a VkPhysicalDevice
@@ -33,7 +44,7 @@ void dxvkExtractVkImage(D3D11Texture2D tex, VkImage* img, uint32_t width, uint32
  * \param [in] physDev The VkPhysicalDevice
  * \returns the adapter index
  */
-int32_t dxvkPhysicalDeviceToAdapterIdx(VkPhysicalDevice);
+int32_t dxvkPhysicalDeviceToAdapterIdx(VkPhysicalDevice dev);
 
 /**
  * \brief DXGI adapter LUID of a VkPhysicalDevice
@@ -42,4 +53,19 @@ int32_t dxvkPhysicalDeviceToAdapterIdx(VkPhysicalDevice);
  * \param [in] physDev the VkPhysicalDevice
  * \param [out] luid the adapter LUID
  */
-void dxvkPhysicalDeviceToAdapterLUID(VkPhysicalDevice, uint64_t* luid);
+void dxvkPhysicalDeviceToAdapterLUID(VkPhysicalDevice dev, uint64_t* luid);
+
+/**
+ * \brief Get Vulkan handles for doing Vulkan operations yourself
+ *
+ * Retrieves the instance, physical device, logical device, graphics queue family, and graphics queue associated with 
+ * the passed D3D11Device.
+ */
+void dxvkGetHandlesForVulkanOps(
+  D3D11Device* dxdev,
+  VkInstance* inst,
+  VkPhysicalDevice* pdev,
+  VkDevice* ldev,
+  uint32_t queueFamily,
+  VkQueue* queue
+);
